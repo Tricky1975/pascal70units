77d0096f490150a72ab6b75d2ab7f62e4243a8e8
Tricky1975 <facebook@tbbs.nl>
2018-03-09 15:17:29 +0100

    Empty shell for Quick JCR6 support

---
+++ QJCR6.pas
@@ -0,0 +1,15 @@
+unit qjcr6;
+
+{
+* This is a very simplistic unit for the JCR6 file system.
+* It can only process non-compressed JCR6 files
+* It has no modular support, so it won't be possible to add any 
+* compression modules to it.
+}
+
+
+interface
+
+
+
+implementation
